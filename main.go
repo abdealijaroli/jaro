9744c843084395223b4b046e215e59ed25e39d32
Abdeali Jaroli <abdealiburu03@gmail.com>
2024-08-08 16:53:19 +0530

    refactor: Update main.go to use command execution for starting the server

--- main.go
+++ main.go
@@ -1,22 +1,26 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"net/http"
 
 	"github.com/a-h/templ"
 	"github.com/abdealijaroli/jaro/web/components"
+
+	"github.com/abdealijaroli/jaro/cmd"
 )
 
 func main() {
 	fs := http.FileServer(http.Dir("web"))
-	http.Handle("/", fs )
+	http.Handle("/", fs)
 
 	http.Handle("/hello", templ.Handler(components.Hello("abdeali", "click me")))
 
-	fmt.Println("listening on :8008")
-	err := http.ListenAndServe(":8008", nil)
-	if err != nil {
-		fmt.Println("Error starting server:", err)
-	}
+	cmd.Execute()
+
+	// fmt.Println("listening on :8008")
+	// err := http.ListenAndServe(":8008", nil)
+	// if err != nil {
+	// 	fmt.Println("Error starting server:", err)
+	// }	
 }
