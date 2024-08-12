1c7321bdab51eeb7ec12ee136988e5dbd99e90dd
Abdeali Jaroli <abdealiburu03@gmail.com>
2024-08-12 11:09:04 +0530

    feat: qr functionality added

--- cmd/shorten.go
+++ cmd/shorten.go
@@ -1,14 +1,32 @@
 package cmd
 
 import (
-	"crypto/sha1"
-	"encoding/hex"
+	"crypto/sha256"
+	"encoding/base64"
 	"fmt"
+
+	"github.com/skip2/go-qrcode"
+
+	"github.com/abdealijaroli/jaro/store"
 )
 
-func ShortenURL(url string) string {
-	h := sha1.New()
-	h.Write([]byte(url))
-	shortURL := hex.EncodeToString(h.Sum(nil))[:5]
-	return fmt.Sprintf("https://jaro.li/%s", shortURL)
+func ShortenURL(url string, storage *store.PostgresStore) (string, error) {
+	hash := sha256.Sum256([]byte(url))
+	shortCode := base64.URLEncoding.EncodeToString(hash[:])[:6]
+	shortURL := fmt.Sprintf("https://jaro.li/%s", shortCode)
+
+	err := storage.CreateShortURL(url, shortCode)
+	if err != nil {
+		return "", err
+	}
+
+	qr, err := qrcode.New(shortURL, qrcode.Medium)
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Println("QR Code:")
+	fmt.Println(qr.ToSmallString(false))
+
+	return shortURL, nil
 }
