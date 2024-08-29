ca38119b8f04e1398fb305bfe8c4825a3be5619f
Abdeali Jaroli <abdealiburu03@gmail.com>
2024-08-29 08:16:19 +0530

    fix: Remove commented out code and unused imports in main.go

--- main.go
+++ main.go
@@ -1,44 +1,44 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+	// "fmt"
+	// "log"
+	// "net/http"
 
-	"github.com/a-h/templ"
+	// "github.com/a-h/templ"
 
-	// "github.com/abdealijaroli/jaro/auth"
-	"github.com/abdealijaroli/jaro/api"
+	// // "github.com/abdealijaroli/jaro/auth"
+	// "github.com/abdealijaroli/jaro/api"
 	"github.com/abdealijaroli/jaro/cmd"
-	"github.com/abdealijaroli/jaro/store"
-	"github.com/abdealijaroli/jaro/web/components"
+	// "github.com/abdealijaroli/jaro/store"
+	// "github.com/abdealijaroli/jaro/web/components"
 )
 
 func main() {
 	// db init
-	storage, err := store.NewPostgresStore()
-	if err != nil {
-		log.Fatalf("Error connecting to database: %v", err)
-	}
-	defer storage.Close()
+	// storage, err := store.NewPostgresStore()
+	// if err != nil {
+	// 	log.Fatalf("Error connecting to database: %v", err)
+	// }
+	// defer storage.Close()
 
-	if err := storage.Init(); err != nil {
-		log.Fatalf("Error initializing database: %v", err)
-	}
+	// if err := storage.Init(); err != nil {
+	// 	log.Fatalf("Error initializing database: %v", err)
+	// }
 
-	fs := http.FileServer(http.Dir("web"))
-	http.Handle("/", fs)
+	// fs := http.FileServer(http.Dir("web"))
+	// http.Handle("/", fs)
 
-	http.Handle("/hello", templ.Handler(components.Hello("hi", "click me")))
+	// http.Handle("/hello", templ.Handler(components.Hello("hi", "click me")))
 
-	http.HandleFunc("POST /signup", func(w http.ResponseWriter, r *http.Request) {
-		api.AddUserToWaitlist(w, r, storage)
-	})
+	// http.HandleFunc("POST /signup", func(w http.ResponseWriter, r *http.Request) {
+	// 	api.AddUserToWaitlist(w, r, storage)
+	// })
 	// http.HandleFunc("/shorten", auth.AuthMiddleware(api.ShortenURL))
 	// http.HandleFunc("/transfer", auth.AuthMiddleware(api.TransferFile))
 
 	cmd.Execute()
 
-	fmt.Println("Server is running on port 8008")
-	http.ListenAndServe(":8008", nil)
+	// fmt.Println("Server is running on port 8008")
+	// http.ListenAndServe(":8008", nil)
 }
