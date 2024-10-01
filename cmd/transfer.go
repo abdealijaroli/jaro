a201de952a118c33cbd7700760bcb414a825ee48
Abdeali Jaroli <abdealiburu03@gmail.com>
2024-10-01 08:59:39 +0530

    Refactor transfer.go to remove unnecessary code and comments

--- cmd/transfer.go
+++ cmd/transfer.go
@@ -3,9 +3,6 @@ package cmd
 import (
 	"log"
 
-	"github.com/gorilla/websocket"
-
-	"github.com/abdealijaroli/jaro/cmd/stream"
 	"github.com/abdealijaroli/jaro/cmd/utils"
 	"github.com/abdealijaroli/jaro/cmd/webrtcconn"
 	"github.com/abdealijaroli/jaro/store"
@@ -16,7 +13,7 @@ func TransferFile(filePath string, store *store.PostgresStore) {
     utils.GenerateQRCode(shortURL)
 
     pc := webrtcconn.CreatePeerConnection()
-    log.Printf("Peer connection created for %s\n", roomID)
+    // log.Printf("Peer connection created for %s\n", roomID)
 
     offer, err := pc.CreateOffer(nil)
     if err != nil {
@@ -28,33 +25,9 @@ func TransferFile(filePath string, store *store.PostgresStore) {
         log.Fatalf("Failed to set local description: %v", err)
     }
 
-    log.Printf("Local description set for %s\n", roomID)
-
-    // Establish WebSocket connection for signaling
-    conn, _, err := websocket.DefaultDialer.Dial("ws://localhost:8080/ws", nil)
-    if err != nil {
-        log.Fatalf("Failed to connect to signaling server: %v", err)
-    }
-    defer conn.Close()
-
-    // Create the room on the signaling server
-    err = stream.CreateRoom(conn, roomID)
-    if err != nil {
-        log.Fatalf("Failed to create room: %v", err)
-    }
-
-    // Send the offer to the signaling server
-    conn.WriteJSON(map[string]string{"type": "offer", "room": roomID, "sdp": offer.SDP})
-
-    log.Printf("Signaling connection established for %s\n", roomID)
+    // log.Printf("Local description set for %s\n", roomID)
 
     // Handle signaling messages
     go webrtcconn.HandleSignaling(pc, roomID)
-
-	
-
     // stream.InitiateTransfer(conn, filePath, roomID, store)
 }
-// func ReceiveFile(roomID string) {
-// 	stream.ReceiveFile(roomID)
-// }
