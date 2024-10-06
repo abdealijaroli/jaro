e7a1bb1fc116d4c66ec8519bb32979ebc54d73b7
Abdeali Jaroli <abdealiburu03@gmail.com>
2024-10-06 12:55:42 +0530

    Refactor transfer.go to improve file transfer handling and add shortURL to database

--- cmd/transfer.go
+++ cmd/transfer.go
@@ -4,30 +4,16 @@ import (
 	"log"
 
 	"github.com/abdealijaroli/jaro/cmd/utils"
-	"github.com/abdealijaroli/jaro/cmd/webrtcconn"
 	"github.com/abdealijaroli/jaro/store"
 )
 
 func TransferFile(filePath string, store *store.PostgresStore) {
-    shortURL, roomID := utils.GenerateShortCode(filePath)
-    utils.GenerateQRCode(shortURL)
+	shortURL, shortCode := utils.GenerateShortCode(filePath)
+	utils.GenerateQRCode(shortURL)
 
-    pc := webrtcconn.CreatePeerConnection()
-    // log.Printf("Peer connection created for %s\n", roomID)
-
-    offer, err := pc.CreateOffer(nil)
-    if err != nil {
-        log.Fatalf("Failed to create offer: %v", err)
-    }
-
-    err = pc.SetLocalDescription(offer)
-    if err != nil {
-        log.Fatalf("Failed to set local description: %v", err)
-    }
-
-    // log.Printf("Local description set for %s\n", roomID)
-
-    // Handle signaling messages
-    go webrtcconn.HandleSignaling(pc, roomID)
-    // stream.InitiateTransfer(conn, filePath, roomID, store)
+	// add shortURL to database
+	err := store.AddShortURLToDB(filePath, shortCode, true)
+	if err != nil {
+		log.Printf("Error adding shortURL to database: %v\n", err)
+	}
 }
